2edfc762eebf7737ab6dad6d5825273cccecfb38
François Wouts <f@zenc.io>
2021-09-24 08:11:44 +1000

    Upgrade to latest Vite beta

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactPlugin from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), svgrPlugin()],
+  plugins: [reactPlugin(), svgrPlugin()],
 });
