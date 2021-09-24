c63b541c1931b10c822c9bde743ba12082bb5669
François Wouts <f@zenc.io>
2021-09-24 14:21:10 +1000

    Remove unused helper function

--- src/testing/type.ts
+++
@@ -1,6 +0,0 @@
-export function type(input: HTMLInputElement, text: string) {
-  input.value = text;
-  // @ts-ignore See https://github.com/facebook/react/issues/11488
-  input._valueTracker.setValue("");
-  input.dispatchEvent(new Event("input", { bubbles: true }));
-}
