9db3688d24a0c3e5996b48c52c75bd6c8ce74867
François Wouts <f@zenc.io>
2022-04-15 12:57:31 +1000

    Set up Preview.js CLI

--- preview.config.js
+++ preview.config.js
@@ -2,9 +2,5 @@ const { reactFrameworkPlugin } = require("@previewjs/plugin-react");
 
 /** @type {import("@previewjs/config").PreviewConfig} */
 module.exports = {
-  // frameworkPlugin: reactFrameworkPlugin.create({
-  //   svgr: {
-  //     componentName: "default",
-  //   },
-  // }),
+  frameworkPlugin: reactFrameworkPlugin.create(),
 };
