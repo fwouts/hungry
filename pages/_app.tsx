31d9855cc07ac8f3cb5bc1973f6d5b57f53252ec
François Wouts <f@zenc.io>
2022-05-25 06:10:55 +1000

    Add progress bar on navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import React from "react";
+import NextNProgress from "nextjs-progressbar";
 import { AppState, AppStateContext } from "../AppState";
 import { RESTAURANT_LIST } from "../data";
 import { ScreenContainer } from "../screens/ScreenContainer";
@@ -8,6 +8,7 @@ import "../styles/globals.css";
 export default function HungryApp({ Component, pageProps }: AppProps) {
   return (
     <ScreenContainer>
+      <NextNProgress />
       <AppStateContext.Provider value={new AppState(RESTAURANT_LIST)}>
         <Component {...pageProps} />
       </AppStateContext.Provider>
