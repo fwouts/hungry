94e73df4d06e28497d51f1ce2c10171d22b31e2d
François Wouts <f@zenc.io>
2022-05-25 21:58:11 +1000

    Simplify Wrapper

--- __previewjs__/Wrapper.tsx
+++ __previewjs__/Wrapper.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { AppState, AppStateContext } from "../AppState";
-import { RESTAURANT_LIST } from "../pages/api/hardcoded-data";
 import { ScreenContainer } from "../screens/ScreenContainer";
 import "../styles/globals.css";
-
-const state = new AppState(RESTAURANT_LIST);
 
 export const Wrapper: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  return (
-    <ScreenContainer>
-      <AppStateContext.Provider value={state}>
-        <div>{children}</div>
-      </AppStateContext.Provider>
-    </ScreenContainer>
-  );
+  return <ScreenContainer>{children}</ScreenContainer>;
 };
