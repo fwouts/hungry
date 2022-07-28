f6c27b806ebbfd74fbc7748e1f56f830e097842a
François Wouts <f@zenc.io>
2022-07-29 09:52:28 +1000

    Remove Wrapper which is no longer needed

--- __previewjs__/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ScreenContainer } from "../screens/ScreenContainer";
-import "../styles/globals.css";
-
-export const Wrapper: React.FC<{
-  children: React.ReactNode;
-}> = ({ children }) => {
-  return <ScreenContainer>{children}</ScreenContainer>;
-};
