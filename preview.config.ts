88972b079bd56a189059b3a2eb6e1403872d2b65
François Wouts <f@zenc.io>
2023-10-09 08:55:03 +1100

    Use `defineConfig` from `@previewjs/config`

---
+++ preview.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "@previewjs/config";
+import { svgr } from "vite-plugin-react-svgr";
+
+export default defineConfig({
+  vite: {
+    plugins: [
+      svgr({
+        exportAs: "ReactComponent",
+      }),
+    ],
+    define: {
+      "process.env.__NEXT_HAS_REWRITES": JSON.stringify(false),
+      "process.env.__NEXT_I18N_SUPPORT": JSON.stringify(false),
+      "process.env.__NEXT_MANUAL_CLIENT_BASE_PATH": JSON.stringify(""),
+      "process.env.__NEXT_NEW_LINK_BEHAVIOR": JSON.stringify(0),
+      "process.env.__NEXT_ROUTER_BASEPATH": JSON.stringify(""),
+      "process.env.__NEXT_SCROLL_RESTORATION": JSON.stringify(null),
+      "process.env.__NEXT_TRAILING_SLASH": JSON.stringify(false),
+    },
+  },
+});
